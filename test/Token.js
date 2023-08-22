ad88d9759e02d4e5df68ef5afe32b8ce9cc28182
Bryan B <bryanj1701@icloud.com>
2023-08-21 23:21:59 -0400

    4. Token Balances

--- test/Token.js
+++ test/Token.js
@@ -7,10 +7,15 @@ const tokens = (n) => {
 
 describe('Token', () => {
 	let token
+	let deployer
+	let accounts
 
 	beforeEach(async () => {
 		const Token = await ethers.getContractFactory('Token')
 		token = await Token.deploy('Dapp University', 'DAPP', '1000000')
+		
+		accounts = await ethers.getSigners()
+		deployer = accounts[0]
 	})
 
 	describe('Deployment', () => {
@@ -33,6 +38,10 @@ describe('Token', () => {
 
 		it('has correct total supply', async () => {
 			expect(await token.totalSupply()).to.equal(totalSupply)
+		})
+
+		it('assigns total supply to deployer', async () => {
+			expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)
 		})
 	})
 })
