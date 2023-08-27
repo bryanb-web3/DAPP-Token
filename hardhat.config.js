3df5495dd72508145a4f68f00fa3b1215ccb4e4d
Bryan B <bryanj1701@icloud.com>
2023-08-27 02:25:30 -0400

    Project Deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,7 +8,7 @@ module.exports = {
   networks: {
     localhost: {},
     goerli: {
-      url: 'https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}',
+      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
       accounts: privateKeys.split(','),
     }
   },
